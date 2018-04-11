78c25465ec4bfc626e8c4b93b5b32e7608b3a009
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-04-10 19:50:26 -0700

    changed electric mucus color

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ document.getElementById('textinputid').addEventListener("input", event => {
 
     const isCorrect = userInput.toLowerCase() === "electric mucus";
 
-    buttonElement.style.backgroundColor = isCorrect ? "#80ffaa": userInput;
+    buttonElement.style.backgroundColor = isCorrect ? "#74E2A2": userInput;
     buttonElement.disabled = !isCorrect;
 });
 
 // ternary for everything - I like this better
-// buttonElement.style.backgroundColor = userInput.toLowerCase() === "electric mucus" ? "#80ffaa" : userInput;
+// buttonElement.style.backgroundColor = userInput.toLowerCase() === "electric mucus" ? "#74E2A2" : userInput;
 // buttonElement.disable = true;
 
 // first attempt
