d64c82e8fa605e0909832a19695a005ab6c3f057
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-04-10 19:40:14 -0700

    coded through js for changing button color based on input

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+document.getElementById('textinputid').addEventListener("input", event => {
+    const buttonElement = document.getElementById('buttonid');
+    
+    console.log(event);
+    const userInput = event.target.value;
 
+    const isCorrect = userInput.toLowerCase() === "electric mucus";
+
+    buttonElement.style.backgroundColor = isCorrect ? "#80ffaa": userInput;
+    buttonElement.disabled = !isCorrect;
+});
+
+// ternary for everything - I like this better
+// buttonElement.style.backgroundColor = userInput.toLowerCase() === "electric mucus" ? "#80ffaa" : userInput;
+// buttonElement.disable = true;
+
+// first attempt
+// const colorInput = document.getElementById('textinputid');
+// colorInput.addEventListener('keydown', function(e) {?(event.target.id)});
